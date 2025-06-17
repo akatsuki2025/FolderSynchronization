42722b6c789e46f165c467d57c1a111662607d03
Natalia Spyra-Stanuch <natspyra@gmail.com>
2025-06-17 13:39:14 +0200

    Add tests for AccessValidator

---
+++ FolderSynchronization.Tests/Validators/AccessValidatorTests.cs
@@ -0,0 +1,118 @@
+﻿using FolderSynchronization.Validators;
+using FolderSynchronization.Exceptions;
+using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
+
+
+namespace FolderSynchronization.Tests.Validators
+{
+    public class AccessValidatorTests : IDisposable
+    {
+        private readonly MockFileSystem _mockFileSystem;
+        private readonly string _testDirectory;
+
+        public AccessValidatorTests()
+        {
+            _mockFileSystem = new MockFileSystem();
+            AccessValidator.SetFileSystem(_mockFileSystem);
+            _testDirectory = Path.Combine(Path.GetTempPath(), $"AccessValidatorTests_{Guid.NewGuid()}");
+            _mockFileSystem.AddDirectory(_testDirectory);
+        }
+
+        public void Dispose()
+        {
+            if (_mockFileSystem.Directory.Exists(_testDirectory))
+            {
+                try
+                {
+                    _mockFileSystem.Directory.Delete(_testDirectory, recursive: true);
+                }
+                catch (IOException)
+                {
+                    foreach (var file in _mockFileSystem.Directory.GetFiles(_testDirectory, "*.*", SearchOption.AllDirectories))
+                    {
+                        _mockFileSystem.File.Delete(file);
+                    }
+                    foreach (var dir in _mockFileSystem.Directory.GetDirectories(_testDirectory, "*", SearchOption.AllDirectories))
+                    {
+                        _mockFileSystem.Directory.Delete(dir);
+                    }
+                }
+            }
+            AccessValidator.ResetFileSystem();
+        }
+
+        [Fact]
+        public void ValidateReadAccess_WithValidPath_DoesNotThrowException()
+        {
+            _mockFileSystem.AddFile(Path.Combine(_testDirectory, "test.txt"), new MockFileData(""));
+
+            var exception = Record.Exception(() => AccessValidator.ValidateReadAccess(_testDirectory));
+
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void ValidateReadAccess_WithInvalidPath_ThrowsValidationException()
+        {
+            string nonExistentPath = Path.Combine(_testDirectory, "NonExistetntFolder");
+            string expectedError = "No read access";
+
+            var exception = Assert.Throws<ValidationException>(() => AccessValidator.ValidateReadAccess(nonExistentPath));
+
+            Assert.Contains(expectedError, exception.Message);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void ValidateReadAccess_WithInvalidInput_ThrowsValidationException(string path)
+        {
+            string expectedError = "No read access";
+
+            var exception = Assert.Throws<ValidationException>(() => AccessValidator.ValidateReadAccess(path));
+
+            Assert.Contains(expectedError, exception.Message);
+
+        }
+
+        [Fact]
+        public void ValidateWriteAccess_WithValidPath_DoesNotThrowException()
+        {
+            var exception = Record.Exception(() => AccessValidator.ValidateWriteAccess(_testDirectory));
+
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void ValidateWriteAccess_WithNonExistentPath_ThrowsValidationException()
+        {
+            string nonExistentPath = Path.Combine(_testDirectory, "NonExistetntFolder");
+            string expectedError = "No write access";
+
+            var exception = Assert.Throws<ValidationException>(() => AccessValidator.ValidateWriteAccess(nonExistentPath));
+
+            Assert.Contains(expectedError, exception.Message);
+        }
+
+        [Fact]
+        public void ValidateWriteAccess_WithReadOnlyDirectory_ThrowsValidationException()
+        {
+            string readOnlyDirectory = Path.Combine(_testDirectory, "ReadOnlyDirectory");
+            _mockFileSystem.AddDirectory(readOnlyDirectory);
+            _mockFileSystem.File.SetAttributes(readOnlyDirectory, FileAttributes.ReadOnly);
+            string expectedError = "No write access";
+
+            try
+            {
+                var exception = Assert.Throws<ValidationException>(() => AccessValidator.ValidateWriteAccess(readOnlyDirectory));
+
+                Assert.Contains(expectedError, exception.Message);
+            }
+            finally
+            {
+                _mockFileSystem.File.SetAttributes(readOnlyDirectory, FileAttributes.Normal);
+            }
+        }
+    }
+}
--- FolderSynchronization/Validators/AccessValidator.cs
+++ FolderSynchronization/Validators/AccessValidator.cs
@@ -4,16 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FolderSynchronization.Exceptions;
+using System.IO.Abstractions;
 
 namespace FolderSynchronization.Validators
 {
     public static class AccessValidator
     {
+        private static readonly IFileSystem _defaultFileSystem;
+        private static IFileSystem _currentFileSystem;
+
+        static AccessValidator()
+        {
+            _defaultFileSystem = new FileSystem();
+            _currentFileSystem = _defaultFileSystem;
+        }
+
+        private static IFileSystem FileSystem
+        {
+            get => _currentFileSystem ?? _defaultFileSystem;
+        }
+
+        public static void SetFileSystem(IFileSystem fileSystem)
+        {
+            _currentFileSystem = fileSystem ?? throw new ArgumentNullException(nameof(fileSystem));
+        }
+
+        public static void ResetFileSystem()
+        {
+            _currentFileSystem = _defaultFileSystem;
+        }
+
         public static void ValidateReadAccess(string path)
         {
             try
             {
-                Directory.GetFiles(path);
+                FileSystem.Directory.GetFiles(path);
             }
             catch (Exception ex) 
             {
@@ -26,12 +51,12 @@ namespace FolderSynchronization.Validators
             try
             {
                 string testFile = Path.Combine(path, "write_test.tmp");
-                File.WriteAllText(testFile, string.Empty);
-                File.Delete(testFile);
+                FileSystem.File.WriteAllText(testFile, string.Empty);
+                FileSystem.File.Delete(testFile);
             }
             catch (Exception ex)
             {
-                throw new ValidationException($"No write acces: {ex.Message}");
+                throw new ValidationException($"No write access: {ex.Message}");
             }
         }
     }
