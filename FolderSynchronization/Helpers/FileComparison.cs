fbe9e478c129691a1bd8597d4e8b97cd15892837
Natalia Spyra-Stanuch <natspyra@gmail.com>
2025-06-18 16:59:29 +0200

    Storing unchanged md5 hashes from source directory in dictionary

--- FolderSynchronization/Helpers/FileComparison.cs
+++ FolderSynchronization/Helpers/FileComparison.cs
@@ -4,53 +4,97 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO.Hashing;
 
 namespace FolderSynchronization.Helpers
 {
     internal static class FileComparison
     {
+        private static readonly Dictionary<string, (string Hash, DateTime LastWriteTimeUtc, long FileSize, uint Crc32)> _hashCache = new();
+        private const int BUFFER_SIZE = 8192; // 8KB buffer
+
         public static bool CheckFileMd5(string file1, string file2)
         {
-            using var md5First = MD5.Create();
-            using var md5Second = MD5.Create();
-            using var stream1 = File.OpenRead(file1);
-            using var stream2 = File.OpenRead(file2);
+            var sourceFileInfo = new FileInfo(file1);
+            string sourceHash = GetSourceFileHash(file1, sourceFileInfo);
+            string destinationHash = CalculateMd5(file2);
 
-            const int bufferSize = 8192; // 8KB chunks
-            var buffer1 = new byte[bufferSize];
-            var buffer2 = new byte[bufferSize];
+            return sourceHash == destinationHash;
+        }
 
-            int bytesRead;
-            while ((bytesRead = stream1.Read(buffer1, 0, bufferSize)) > 0)
+        private static string GetSourceFileHash(string filePath, FileInfo fileInfo)
+        {
+            if (!_hashCache.TryGetValue(filePath, out var cachedData))
             {
-                int read2 = stream2.Read(buffer2, 0, bufferSize);
-                if (bytesRead != read2)
-                    return false;
-
-                md5First.TransformBlock(buffer1, 0, bytesRead, buffer1, 0);
-                md5Second.TransformBlock(buffer2, 0, read2, buffer2, 0);
+                string hash = CalculateMd5(filePath);
+                _hashCache[filePath] = (hash, fileInfo.LastAccessTimeUtc, fileInfo.Length, CalculateCrc32(filePath));
+                return hash;
             }
 
-            if (stream2.Read(buffer2, 0, bufferSize) > 0)
-                return false;
+            bool sameTimestamp = cachedData.LastWriteTimeUtc.Equals(fileInfo.LastWriteTimeUtc);
+            bool sameSize = cachedData.FileSize == fileInfo.Length;
 
-            md5First.TransformFinalBlock(Array.Empty<byte>(), 0, 0);
-            md5Second.TransformFinalBlock(Array.Empty<byte>(), 0, 0);
+            if (!sameTimestamp || !sameSize)
+            {
+                string hash = CalculateMd5(filePath);
+                uint crc = CalculateCrc32(filePath);
+                _hashCache[filePath] = (hash, fileInfo.LastWriteTimeUtc, fileInfo.Length, crc);
+                return hash;
+            }
 
-            var hash1 = md5First.Hash;
-            var hash2 = md5Second.Hash;
+            uint currentCrc = CalculateCrc32(filePath);
+            if (currentCrc == cachedData.Crc32)
+            {
+                return cachedData.Hash;
+            }
 
-            if (hash1 is null || hash2 is null)
+            string newHash = CalculateMd5(filePath);
+            _hashCache[filePath] = (newHash, fileInfo.LastWriteTimeUtc, fileInfo.Length, currentCrc);
+            return newHash;
+        }
+
+        private static string CalculateMd5(string filePath)
+        {
+            using var md5 = MD5.Create();
+            using var stream = File.OpenRead(filePath);
+            var buffer = new byte[BUFFER_SIZE];
+            int bytesRead;
+
+            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                md5.TransformBlock(buffer, 0, bytesRead, buffer, 0);
+            }
+
+            md5.TransformFinalBlock(Array.Empty<byte>(), 0, 0);
+
+            byte[]? hash = md5.Hash;
+
+            if (hash is null)
             {
                 throw new InvalidOperationException("MD5 hash computation failed");
             }
 
-            if (hash1.Length != 16 || hash2.Length != 16)
+            if (hash.Length != 16) // MD5 hashes should always be 16 bytes
             {
-                throw new InvalidOperationException("Invalid MD5 hash length");
+                throw new InvalidOperationException($"Invalid MD5 hash length: {hash.Length} bytes");
             }
 
-            return hash1.SequenceEqual(hash2);
+            return Convert.ToBase64String(hash);
+        }
+
+        private static uint CalculateCrc32(string filename)
+        {
+            using var stream = File.OpenRead(filename);
+            var crc32 = new Crc32();
+            var buffer = new byte[BUFFER_SIZE];
+            int bytesRead;
+
+            while((bytesRead = stream.Read(buffer,0, buffer.Length)) > 0)
+            {
+                crc32.Append(buffer.AsSpan(0, bytesRead));
+            }
+
+            return BitConverter.ToUInt32(crc32.GetCurrentHash());
         }
     }
 }
